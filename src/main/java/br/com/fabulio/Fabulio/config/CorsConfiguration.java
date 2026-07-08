39a23161c399d8cb4a2df9568e94aaddcee264a9
Gabriel <gabriellopesp@hotmail.com>
2026-07-08 11:18:02 -0300

    fix: Adicionando CORS para GitHub Pages

--- src/main/java/br/com/fabulio/Fabulio/config/CorsConfiguration.java
+++ src/main/java/br/com/fabulio/Fabulio/config/CorsConfiguration.java
@@ -10,7 +10,11 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                .allowedOrigins("http://127.0.0.1:5500",
+                        "https://gabrielpree.github.io",
+                        "http://localhost:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
