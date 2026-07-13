1e3624c844af0d09838e15b45a1a5a65ffff8155
Gabriel <gabriellopesp@hotmail.com>
2026-07-13 18:35:11 -0300

    Adicionado um health controller para checar status da api

---
+++ src/main/java/br/com/fabulio/Fabulio/controller/HealthController.java
@@ -0,0 +1,13 @@
+package br.com.fabulio.Fabulio.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/api/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Backend está funcionando corretamente!");
+    }
+}
